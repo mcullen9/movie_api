7e15def9a4260fd43bdb37e43dae1b8c796ad945
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2024-01-22 22:01:59 -0500

    Update index.js
    added put request to update a user's info

--- index.js
+++ index.js
@@ -203,6 +203,21 @@ app.post('/users', (req, res) => {
     }
 })
 
+// UPDATE
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+   let user = users.find( user => user.id == id);
+
+   if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+   } else {
+    res.status(400).send('no such user')
+   }
+})
+
 // READ
 app.get('/movies', (req, res) => {
     res.status(200).json(movies);
