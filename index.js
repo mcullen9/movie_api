ee6affe7b7e8310448b49c09e28ceefb309d3878
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2024-01-27 00:37:08 -0500

    Update index.js
    get a user by username using Mongoose's READ

--- index.js
+++ index.js
@@ -234,6 +234,19 @@ app.post('/users', async (req, res) => {
       });
   });
 
+// READ 
+// GET a user by username
+app.get('/users/:Username', async (req, res) => {
+    await Users.findOne({ Username: req.params.Username })
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+
 // UPDATE
 app.put('/users/:id', (req, res) => {
     const { id } = req.params;
