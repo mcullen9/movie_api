2275cb8c52c98efc5c9a0325b9b0426874d79bf7
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2024-02-05 04:14:34 -0500

    edit syntax

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 //mongoose.connect('mongodb://localhost:27017/mfDB', { useNewUrlParser: true, useUnifiedTopology: true }); 
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true }); 
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
   
 
 
