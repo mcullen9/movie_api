e4fe498662c142c56bb61013a4b231cb78641d35
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2024-02-08 23:08:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ app.post(
       return res.status(422).json({ errors: errors.array() });
     }
 
-    let hashedPassword = Users.hashedPassword(req.body.Password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
     await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
       .then((user) => {
         if (user) {
