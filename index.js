1cc4ef6e7ed39b732d56194edc08e4faea7adb1c
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2024-03-18 05:41:59 -0400

    Update index.js
    debug

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ app.post(
   async (req, res) => {
     await Users.findOneAndUpdate(
       { Username: req.params.Username },
-      { $push: { FavoriteMovies: req.params.Title } }, //should this say Title or MovieID?
+      { $push: { FavoriteMovies: req.params.MovieID } }, //should this say Title or MovieID?
       { new: true }
     )
       .then((updatedUser) => {
@@ -314,10 +314,10 @@ app.delete(
   "/users/:Username/movies/:Title",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
-    await Users.findOneAndUpdate(
+    await Users.findOneAndDelete(
       { Username: req.params.Username },
       {
-        $pull: { FavoriteMovies: req.params.Title }, //MovieID or Title?
+        $pull: { FavoriteMovies: req.params.MovieID }, //MovieID or Title?
       },
       { new: true }
     )
